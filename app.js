3f96330dcfcb8ad615f38a16adda60340321fdc9
Francesco Pezzati <fpezzati@gmail.com>
2016-10-05 00:26:34 +0200

    Facing iron-ajax.

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ console.log("__dirname = "+__dirname);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/srv/offer/read/all', function(req, res){
-  res.sendFile('./mock/offer.js', {root: __dirname});
+  res.sendFile('./mock/offerset.js', {root: __dirname});
+});
+app.get('/srv/offer/read/:id', function(req, res) {
+  res.sendFile('./mock/offersingle.js', {root: __dirname});
 });
 app.listen(80, function(){});
