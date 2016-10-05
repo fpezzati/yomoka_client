6395dca1d0121bc9bc6a6fc7fbc8049280020c1b
Francesco Pezzati <fpezzati@gmail.com>
2016-10-05 23:19:15 +0200

    Fixed an issue in my .bowerrc.

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ console.log("__dirname = "+__dirname);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/srv/offer/read/all', function(req, res){
+  console.log("get offer set.");
   res.sendFile('./mock/offerset.js', {root: __dirname});
 });
 app.get('/srv/offer/read/:id', function(req, res) {
+  console.log("get single offer.");
   res.sendFile('./mock/offersingle.js', {root: __dirname});
 });
 app.listen(80, function(){});
