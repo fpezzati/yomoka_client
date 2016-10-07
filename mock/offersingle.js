ef64880c83a7f3a01323e40219d0621426a8c45b
francesco <francesco.pezzati@iia.cnr.it>
2016-10-07 16:44:28 +0200

    Fixed an issue about mock json data.

--- mock/offersingle.js
+++ mock/offersingle.js
@@ -1,2 +1,1 @@
-{ "_id" : ObjectId("57e2eae8860278f2d1a03ff7"), "expires" : ISODate("2016-09-12T08:00:00Z"), "crs" : 0, "owner_id" : ObjectId("57e2eae8860278f2d1a03ff6"), "latitude
-" : 43.770166, "description" : "tasty tomatoes", "longitude" : 11.208077 }
+{ "_id" : "57e2eae8860278f2d1a03ff7", "expires" : "2016-09-12T08:00:00Z", "crs" : 0, "owner_id" : "57e2eae8860278f2d1a03ff6", "latitude" : 43.770166, "description" : "tasty tomatoes", "longitude" : 11.208077 }
