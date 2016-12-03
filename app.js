e0b52e96c3081c0aa4c43f795e4c9527d0cd63f4
Francesco Pezzati <fpezzati@gmail.com>
2016-12-03 10:54:18 +0100

    Ok. yo-offer-set and yo-offer are 100% working. Maps are 0%.

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
-
-var database = {};
 app.use(express.static(__dirname + '/public'));
 
 app.get('/srv/offer/all', function(req, res){
   console.log("get offer set.");
-  res.sendFile('./mock/offerset.js', {root: __dirname});
+  var dataset = new Array();
+  for (var key in database) {
+    dataset.push(database[key]);
+  }
+  res.send(dataset);
 });
 app.get('/srv/offer/:id', function(req, res) {
   console.log("read offer: "+req.params.id);
@@ -36,22 +38,24 @@ app.post('/srv/offer', function(req, res) {
 app.put('/srv/offer', function(req, res) {
   console.log("update offer: "+req.body);
   if(req.body._id in database) {
-      database[req.body._id] = req.body;
+      database[req.body._id] = JSON.stringify(req.body);
   } else {
-    res.sent('{"description": "NONE"}');
+    res.send('{"description": "NONE"}');
   }
 });
 app.delete('/srv/offer/:id', function(req, res) {
   console.log("delete offer.id: "+req.params.id);
-  if(req.body._id in database) {
-      delete database[req.body._id];
+  if(req.params.id in database) {
+      delete database[req.params.id];
   } else {
-    res.sent('{"description": "NONE"}');
+    res.send('{"description": "NONE"}');
   }
 });
 app.listen(80, function(){});
 
-var initDb = function() {
+var initDb = function(database) {
     database["57e2eae8860278f2d1a03ff7"] = { "_id" : "57e2eae8860278f2d1a03ff7", "expires" : "2016-09-12T08:00:00Z", "crs" : 0, "owner_id" : "57e2eae8860278f2d1a03ff6", "latitude" : 43.770166, "description" : "tasty tomatoes", "longitude" : 11.208077 };
     database["57e2eb59860296dfe44ee9eb"] = { "_id" : "57e2eb59860296dfe44ee9eb", "expires" : "2016-09-12T08:00:00Z", "crs" : 0, "owner_id" : "57e2eb59860296dfe44ee9ea", "latitude" : 44.770166, "description" : "delicious peaches", "longitude" : 10.208077 };
 };
+var database = {};
+initDb(database);
